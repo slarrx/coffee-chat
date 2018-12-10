ad10e010093558e247da00b2ac77f50902fe3c22
slarrx <slarrx@gmail.com>
2018-12-10 13:27:00 +0700

    Added constructors for server/client

--- app/src/client/client.cc
+++ app/src/client/client.cc
@@ -1,9 +1,25 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <cerrno>
+#include <stdexcept>
+
 #include "client.h"
 
 namespace coffee_chat {
 
 Client::Client(char* ip, int port) {
+  sockaddr_in address = {0};
+  address.sin_family = AF_INET;
+  address.sin_port = htons(static_cast<uint16_t>(port));
+  if (!inet_aton(ip, &address.sin_addr)) {
+    errno = EINVAL;
+    throw std::invalid_argument("inet_aton()");
+  }
 
+  if (connect(socket_, (sockaddr*)&address, sizeof(address)) < 0) {
+    throw std::runtime_error("connect()");
+  }
 }
 
 void Client::Run() {
--- app/src/server/server.cc
+++ app/src/server/server.cc
@@ -1,9 +1,25 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <cerrno>
+#include <stdexcept>
+
 #include "server.h"
 
 namespace coffee_chat {
 
 Server::Server(int port) {
+  sockaddr_in address = {0};
+  address.sin_family = AF_INET;
+  address.sin_port = htons(static_cast<uint16_t>(port));
+  address.sin_addr.s_addr = INADDR_ANY;
 
+  if (bind(socket_, (sockaddr*)&address, sizeof(address)) < 0) {
+    throw std::runtime_error("bind()");
+  }
+
+  if (listen(socket_, 5) < 0) {
+    throw std::runtime_error("listen()");
+  }
 }
 
 void Server::Run() {
